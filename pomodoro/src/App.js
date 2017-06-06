56efed1beb66e7633571d69d163eb8bcc60d746b
Taylor Rohrich <taylorrohrich@gmail.com>
2017-06-06 11:17:31 -0400

    finished timer and memo

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {pomodoro} from "./firebase-config";
-import {Button, Layout} from 'antd';
-import * as firebase from 'firebase';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import { pomodoro } from "./firebase-config";
+import { Button, Layout } from "antd";
+import * as firebase from "firebase";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-import Page from './Page'
-import Timer from './Timer'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Page from "./Page";
+import Timer from "./Timer";
 
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -20,29 +16,33 @@ var provider = new firebase.auth.GoogleAuthProvider();
 
 class App extends Component {
   constructor(props) {
-        super(props);
-        this.state = {
-          logInState: true
-        }
-  }     
-  
+    super(props);
+    this.state = {
+      logInState: true
+    };
+  }
+
   handleLogInButton(logInState) {
-    if(this.state.logInState) {
-        firebase.auth().signInWithPopup(provider).then((result) => {
-        this.setState({
-          username: result.user.displayName,
-          emailVerified: result.user.emailVerified,
-          logInState: !this.state.logInState,
-          userID: result.user.uid
+    if (this.state.logInState) {
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(result => {
+          this.setState({
+            username: result.user.displayName,
+            emailVerified: result.user.emailVerified,
+            logInState: !this.state.logInState,
+            userID: result.user.uid
+          });
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          console.log(user);
+
+          // ...
         })
-      // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        console.log(user);
-        
-      // ...
-        }).catch(function(error) {
+        .catch(function(error) {
           // Handle Errors here.
           var errorCode = error.code;
           var errorMessage = error.message;
@@ -52,38 +52,45 @@ class App extends Component {
           var credential = error.credential;
           // ...
         });
-       
-    }
-    else {
-      firebase.auth().signOut().then(() => {
-        this.setState({
-          username: "",
-          emailVerified: false,
-          logInState: !this.state.logInState
+    } else {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.setState({
+            username: "",
+            emailVerified: false,
+            logInState: !this.state.logInState
+          });
+          console.log("log out successful");
+
+          window.location = "/";
         })
-        console.log("log out successful")
-        
-        window.location = "/";
-
-      }).catch(function(error) {
-        // An error happened.
-      });
+        .catch(function(error) {
+          // An error happened.
+        });
     }
-    
   }
-
 
   render() {
     return (
       <div>
         <Layout>
           <Header>
-            <Button type="primary" onClick={() => this.handleLogInButton(this.state.logInState)}>
-              {this.state.logInState && <div>Log In</div> }{!this.state.logInState && <div>Log Out</div> }</Button> 
-             
+            <Button
+              type="primary"
+              onClick={() => this.handleLogInButton(this.state.logInState)}
+            >
+              {this.state.logInState && <div>Log In</div>}
+              {!this.state.logInState && <div>Log Out</div>}
+            </Button>
+
           </Header>
           <Layout>
-            <Content className="content"> {!this.state.logInState && <Page userID={this.state.userID}/>}</Content>
+            <Content className="content">
+              {" "}{!this.state.logInState &&
+                <Page userID={this.state.userID} />}
+            </Content>
           </Layout>
           <Layout>
             {/*<Footer>Footer</Footer>*/}
--- pomodoro/src/Page.js
+++ pomodoro/src/Page.js
@@ -1,227 +1,266 @@
-import React, { Component } from 'react';
-import './Page.css';
-import './App.css';
-import FlatButton from 'material-ui/FlatButton';
-import RaisedButton from 'material-ui/RaisedButton';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Card, CardText, CardHeader, CardActions } from 'material-ui/Card';
-import TextField from 'material-ui/TextField';
-import * as firebase from 'firebase';
+import React, { Component } from "react";
+import "./Page.css";
+import "./App.css";
+import FlatButton from "material-ui/FlatButton";
+import RaisedButton from "material-ui/RaisedButton";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import { Card, CardText, CardHeader, CardActions } from "material-ui/Card";
+import TextField from "material-ui/TextField";
+import * as firebase from "firebase";
 import {
   Table,
   TableBody,
   TableHeader,
   TableHeaderColumn,
   TableRow,
-  TableRowColumn,
-} from 'material-ui/Table';
+  TableRowColumn
+} from "material-ui/Table";
 
 const cardStyle = {
-    width: "35%"
-}
+  width: "35%"
+};
 
 const timeStyle = {
-    fontSize: "70px"
-}
+  fontSize: "70px"
+};
 
 const memoStyle = {
-    width: "50%",
-}
+  width: "50%"
+};
 
 class Page extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-             count: 1500,
-        half25: 750,
-        half5: 150,
-        work: true,
-        play: true,
-        edit: true,
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1500,
+      half25: 750,
+      half5: 150,
+      work: true,
+      play: true,
+      edit: true,
 
-            memoInput: "",
-            users: []
+      // memo states
+      user: {},
+      listOfMemoItems: []
+    };
+  }
 
-        }
+  componentDidMount() {
+    Notification.requestPermission();
+  }
 
-        this.leaderboard =  []
-    }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
 
-    componentDidMount() {
-        Notification.requestPermission();
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timer)
-    }
-
-    tick() {
-    this.setState({count: (this.state.count + -1)})
-    if (this.state.work){
-    if ((this.state.count) == (this.state.half25)){
-        console.log(2*(this.state.count))
+  tick() {
+    this.setState({ count: this.state.count + -1 });
+    if (this.state.work) {
+      if (this.state.count == this.state.half25) {
+        console.log(2 * this.state.count);
         new Notification("Half Way for " + this.state.name + "!");
-    }}
+      }
+    }
 
-    if (!this.state.work){
-        if ((this.state.count) == (this.state.half5)){
-        console.log(2*(this.state.count))
+    if (!this.state.work) {
+      if (this.state.count == this.state.half5) {
+        console.log(2 * this.state.count);
         new Notification("Half Way for Break!");
-    }}
-    if (this.state.count == 0){
-        if (this.state.work){
-            this.stopTimer();
-            this.setState({
-                ...this.state,
-                work:false,
-                count:300,
-                play:true
-            })
-            new Notification("Time's Up for " + this.state.name + "!");
-        }else if(!this.state.work){
-            this.stopTimer();
-            this.setState({
-                ...this.state,
-                work:true,
-                count:1500,
-                play:true,
-                name: null
-            })
-            new Notification("Time's Up for break! Back to Work");
-        }
+      }
     }
-  }
-
-    format(seconds) {
-        let m = Math.floor(seconds % 3600 / 60);
-        let s = Math.floor(seconds % 3600 % 60);
-        let timeFormated = (m < 10 ? "0" : "") + m + ":" + (s < 10 ? "0" : "") + s;
-        return timeFormated;
-    }
-
-    handleChangeTime(event) {
+    if (this.state.count == 0) {
+      if (this.state.work) {
+        this.stopTimer();
         this.setState({
-            ...this.state,
-            totalTime: event.target.value
+          ...this.state,
+          work: false,
+          count: 300,
+          play: true
         });
-
-    }
-
-
-    startTimer() {
-        clearInterval(this.timer)
-        this.timer = setInterval(this.tick.bind(this), 1000)
+        new Notification("Time's Up for " + this.state.name + "!");
+      } else if (!this.state.work) {
+        this.stopTimer();
         this.setState({
-            ...this.state,
-            play: false
-        })
+          ...this.state,
+          work: true,
+          count: 1500,
+          play: true,
+          name: null
+        });
+        new Notification("Time's Up for break! Back to Work");
+      }
     }
+  }
 
-    stopTimer() {
-        clearInterval(this.timer)
-        this.setState({
-            ...this.state,
-            play: true
-        })
-    }
+  format(seconds) {
+    let m = Math.floor(seconds % 3600 / 60);
+    let s = Math.floor(seconds % 3600 % 60);
+    let timeFormated = (m < 10 ? "0" : "") + m + ":" + (s < 10 ? "0" : "") + s;
+    return timeFormated;
+  }
 
-    resetTimer () {
-    clearInterval(this.timer)  
+  handleChangeTime(event) {
     this.setState({
-        ...this.state,
-        count: 1500,
-        totalTime: null,
-        name:null,
-        work: true,
-        play: true
+      ...this.state,
+      totalTime: event.target.value
     });
   }
 
-    //MEMO Functions
-    handleMemoInput = (event, memoInput) => {
-        this.setState({
-            ...this.state,
-            memoInput: event.target.value
-        })
-    }
-
-    handleSubmitMemo = (memoInput) => {
-        var date = String(new Date());
-        var array1=this.state.users,
-        updatedUsers= array1.concat([{ date: date, memo: memoInput,}]);
-        // this.leaderboard.concat({
-        //     user: this.props.userID,
-        //     num_memos: this.leaderboard.num_memos + 1
-        // })
-        this.writeUserData(updatedUsers)
-    }
-    writeUserData=users=> {
-    firebase.database().ref(this.props.userID).set({
-        users
+  startTimer() {
+    clearInterval(this.timer);
+    this.timer = setInterval(this.tick.bind(this), 1000);
+    this.setState({
+      ...this.state,
+      play: false
     });
-    }
-    componentWillMount(){
-        var readData = firebase.database().ref(this.props.userID);
-        readData.on('value',(snapshot) => {
-            if (snapshot.val() === null) {
-            this.setState({
-            ...this.state,
-            users: [],
-            })}
-            else {
-            this.setState({
-            ...this.state,
-            users: snapshot.val().users,
-            })}
-        })
+  }
 
+  stopTimer() {
+    clearInterval(this.timer);
+    this.setState({
+      ...this.state,
+      play: true
+    });
+  }
 
-    } 
-    render() {
-        
-        return (
-            
-            <MuiThemeProvider>
-                <div>
-                <div className='timer'>
-                    <Card style={cardStyle}>
-                        <CardHeader style={timeStyle}>{this.format(this.state.count)}</CardHeader>
-                        <CardText>   
-                                {(this.state.totalTime != null) && (this.format(this.state.totalTime))}
-                            <TextField style={memoStyle} multiLine={true} hintText="Describe task here." onChange= {this.handleMemoInput} />
-                             <br />
-                            <RaisedButton primary={true} label='Submit' onClick={() => this.handleSubmitMemo(this.state.memoInput)} />
-                        </CardText>
-                        <CardActions>
-                                <FlatButton label={this.state.play ? 'Start' : 'Stop'} onClick={(event) => { this.state.play ? this.startTimer() : this.stopTimer() }} />
-                                <FlatButton label="Reset" onClick={(event) => this.resetTimer()} />
-                        </CardActions>
-                    </Card>
-                </div>
-                <div>
-                    <Card>
-                        <CardHeader> Memo </CardHeader>
-                        <CardText>
-                            <table>
-                                {this.state.users.map((memoObject) => {
-                                    return (
-                                        <tr>
-                                            <td>
-                                                {memoObject.memo}
-                                            </td>
-                                        </tr>
-                                        )
-                                    }
-                                )}                
-                         </table>
-                         </CardText>
-                    </Card>
-                     
-                </div>
-                </div>
-            </MuiThemeProvider>
-        )
-    }
+  resetTimer() {
+    clearInterval(this.timer);
+    this.setState({
+      ...this.state,
+      count: 1500,
+      totalTime: null,
+      name: null,
+      work: true,
+      play: true
+    });
+  }
+
+  //MEMO Functions
+  handleMemoInput = (event, memoInput) => {
+    this.setState({
+      ...this.state,
+      memoInput: event.target.value
+    });
+  };
+
+  handleSubmitMemo = memoInput => {
+    var date = String(new Date());
+    var temp_list_of_memo_items = this.state.listOfMemoItems.splice(0);
+    var memoWithDate = { memoInput: memoInput, date: date };
+    var temp_user = this.state.user;
+    var temp_userID = this.props.userID;
+    var updated_temp_list_of_memo_items = temp_list_of_memo_items.concat([
+      memoWithDate
+    ]);
+    temp_user[temp_userID] = updated_temp_list_of_memo_items;
+    this.writeUserAndMemoData(temp_user);
+  };
+
+  writeUserAndMemoData = temp_user => {
+    firebase.database().ref().set({
+      temp_user
+    });
+  };
+
+  componentWillMount() {
+    var temp_userID = this.props.userID;
+    var readData = firebase.database().ref();
+    readData.on("value", snapshot => {
+      if (snapshot.val() === null) {
+        this.setState({
+          ...this.state,
+          user: {},
+          listOfMemoItems: []
+        });
+      } else {
+        this.setState({
+          ...this.state,
+          user: snapshot.val().temp_user,
+          listOfMemoItems: snapshot.val().temp_user[this.props.userID]
+        });
+      }
+    });
+  }
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <div className="timer">
+            <Card style={cardStyle}>
+              <CardHeader style={timeStyle}>
+                {this.format(this.state.count)}
+              </CardHeader>
+              <CardText>
+                {this.state.totalTime != null &&
+                  this.format(this.state.totalTime)}
+                <TextField
+                  style={memoStyle}
+                  multiLine={true}
+                  hintText="Describe task here."
+                  onChange={this.handleMemoInput}
+                />
+                <br />
+                <RaisedButton
+                  primary={true}
+                  label="Submit"
+                  onClick={() => this.handleSubmitMemo(this.state.memoInput)}
+                />
+              </CardText>
+              <CardActions>
+                <FlatButton
+                  label={this.state.play ? "Start" : "Stop"}
+                  onClick={event => {
+                    this.state.play ? this.startTimer() : this.stopTimer();
+                  }}
+                />
+                <FlatButton
+                  label="Reset"
+                  onClick={event => this.resetTimer()}
+                />
+              </CardActions>
+            </Card>
+            <div className="memoList">
+              <Card>
+                <CardHeader> Memo List </CardHeader>
+                <CardText>
+                  <table>
+                    {this.state.listOfMemoItems.map(memoItem => {
+                      return (
+                        <tr>
+                          <td> {memoItem.memoInput} </td>
+                          <td> {memoItem.date} </td>
+                        </tr>
+                      );
+                    })}
+                  </table>
+                </CardText>
+              </Card>
+            </div>
+
+            <div className="leaderboard">
+              <Card>
+                <CardHeader> Leaderboard </CardHeader>
+                {/*<CardText>
+                  <table>
+                    {this.state.listOfMemoItems.map(memoItem => {
+                      return (
+                        <tr>
+                          <td> {memoItem.memoInput} </td>
+                          <td> {memoItem.date} </td>
+                        </tr>
+                      );
+                    })}
+                  </table>
+                </CardText>*/}
+              </Card>
+            </div>
+
+          </div>
+
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default Page;
